5307cde1ffd884e0a6322c0ccc1258e57385a121
aleycris <aleycris@gmail.com>
2015-06-05 11:59:34 -0300

    Ajuste para nueva version

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -11,14 +11,27 @@
  * VideoPlayer.play("file:///android_asset/www/path/to/my/video.mp4");
  * VideoPlayer.play("https://www.youtube.com/watch?v=en_sVVjWFKk");
  */
+cordova.define("cordova/plugin/videoplayer",
+  function(require, exports, module) {
+    var exec = require("cordova/exec");
+    var VideoPlayer = function () {};
 
-var exec = require("cordova/exec");
-
-var VideoPlayer = {
-    play: function(url) {
+    /**
+     * Starts the video player intent
+     *
+     * @param url           The url to play
+     */
+    VideoPlayer.prototype.play = function(url) {
         exec(null, null, "VideoPlayer", "playVideo", [url]);
-    }
-};
+    };
 
-module.exports = VideoPlayer;
+    var videoPlayer = new VideoPlayer();
+    module.exports = videoPlayer;
+});
 
+if (!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.videoPlayer) {
+    window.plugins.videoPlayer = cordova.require("cordova/plugin/videoplayer");
+}
